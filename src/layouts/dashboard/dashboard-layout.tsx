774adf6990d47798aa364a11b16030589332cb25
farrasnazhif <farrasnazhif@gmail.com>
2026-04-25 10:35:42 +0700

    fix: delete dashboard footer

--- src/layouts/dashboard/dashboard-layout.tsx
+++ src/layouts/dashboard/dashboard-layout.tsx
@@ -1,16 +1,15 @@
-import DashboardFooter from "./dashboard-footer";
 import DashboardNavbar from "./dashboard-navbar";
 import DashboardSidebar from "./dashboard-sidebar";
 
 export default function DashboardLayout({
   withNavbar,
   withSidebar,
-  withFooter,
+
   children,
 }: {
   withNavbar?: boolean;
   withSidebar?: boolean;
-  withFooter?: boolean;
+
   children: React.ReactNode;
 }) {
   return (
@@ -22,8 +21,6 @@ export default function DashboardLayout({
 
         <main className="flex-1 bg-gray-50 p-6">{children}</main>
       </div>
-
-      {withFooter && <DashboardFooter />}
     </div>
   );
 }
